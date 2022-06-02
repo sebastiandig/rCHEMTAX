7785791493d1725cd14644bb970a2d906e9cd388
Sebastian D <sebastian15@usf.edu>
2022-06-02 19:52:40 -0400

    start working on amatfactsd function

---
+++ scripts/amatfactsd.R
@@ -0,0 +1,75 @@
+amatfactsd <- function(x, sdx, b){
+################################################################################
+#                                                                              # 
+#               Calculate Factor a in min(x-ab) using least square             #
+#                                                                              #    
+################################################################################
+# INPUTS:
+#--------
+# input = 
+#         x    Matrix to be factored as a*b
+#         sdx  Standard deviations of x
+#         b    Value of b
+##########
+# OUTPUTS:
+#---------
+# output =  
+#         a    Result left factor of x
+##########
+# -----Notes:-----
+# Orginal: 2010-03-14  Matalb7  W.Whiten
+# -----References:-----
+#
+# -----Author:-----
+# Sebastian Di Geronimo (Thu Jun 02 18:30:06 2022)
+  
+
+  
+# amatfact  Calculate factor a in min(x-ab) using lsqnonneg
+# 
+# ns=size(x,1);
+# nt=size(b,1);
+# 
+# a=zeros(ns,nt);
+# for j=1:ns
+# %        aa=lsqnonneg(bb,x(j,:)'); 
+#     aa=lsqnonneg(b'./repmat(sdx(j,:)',1,nt),(x(j,:)./sdx(j,:))');
+#     a(j,:)=aa';
+# end
+# 
+# return
+# end
+
+library(pracma)
+  x <- matrix(c(0.0372,    0.2869,
+                0.6861,   0.7071,
+                0.6233,    0.6245,
+                0.6344,    0.6170),
+              nrow = 4,
+              ncol = 2)
+  
+  b <- c(0.8587,
+              0.1781,
+              0.0747,
+              0.8405)
+  
+  ns <- dim(x)[1]
+  nt <- length(b)
+  
+  a <- matrix(0, ns, nt)
+  
+  for (j in seq(ns)) {
+    
+  # TODO: figure out the dimensions of sdx ----------------------------------------
+    
+  #  aa=lsqnonneg(b'./    repmat(sdx(j,:)',1,nt),
+  #               (x(j,:)./sdx(j,:))');
+    # sdx(j,:) is row j all columns, repeate
+    # aa <- FUN( Conj(b)/ pracma::repmat(Conj(sdx[j,]),1,nt) , Conj(x[j,] / sdx[j,])  )
+    
+  aa <- pracma::lsqnonneg(Conj(b)/ pracma::repmat(Conj(sdx[j,]),1,nt), 
+                          Conj(x[j,] / sdx[j,]))  
+  a[j,] <- aa$x
+  }
+  
+  }
