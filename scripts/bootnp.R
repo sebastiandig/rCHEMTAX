0d2f37ba8849bb40e81bcfc2098e27346959195e
Sebastian D <sebastian15@usf.edu>
2022-06-10 15:45:54 -0400

    close to finishing this script

--- scripts/bootnp.R
+++ scripts/bootnp.R
@@ -13,13 +13,12 @@ bootnp <- function(s,ssd,f,fsd) {
 # --------
 # OUTPUTS:
 # --------
-#  =
 # Currently none as only plots
 #
 # --------
 # NOTES:
 # --------
-# Original: 2010-03-14  Matalb7  W.Whiten
+# Original: 2010-02-27  Matlab7  W.Whiten
 #
 # --------
 # References:
@@ -28,7 +27,7 @@ bootnp <- function(s,ssd,f,fsd) {
 # --------
 # Author:
 # --------
-# Author (Fri Jun 10 12:01:31 2022)
+# Sebastian Di Geronimo (Fri Jun 10 12:01:31 2022)
   
   
 # function bootnp(s,ssd,f,fsd)
@@ -40,9 +39,17 @@ bootnp <- function(s,ssd,f,fsd) {
 # %  ssd  Matrix of standard deviations for x
 # %  f    Stabilising value for b, non zero locations, & initial value
 # %  fsd  Matrix of standard deviations for b
-# 
+#
+
+  require("ggplot2")
+  require("tictoc")
+  require("pracma")
+  require("rprojroot")
+  
+  root <- rprojroot::find_rstudio_root_file()
+  nrep <-  10  
 # nrep=10;
-# %nrep=20;
+
 # 
 # [ns,np]=size(s); %#ok<NASGU>
 # [nt,np]=size(f);  %#ok<NASGU>
@@ -50,9 +57,22 @@ bootnp <- function(s,ssd,f,fsd) {
 # ni=length(indx);
 # tf=zeros(nrep,ni);
 # tc=zeros(nrep,ns*nt);
-# 
+
 # cc1=zeros(ns,nt);
-# 
+
+  ns   <-  dim(s)[1] # row of s
+  np   <-  dim(s)[2] # col of s
+  nt   <-  dim(f)[1] # row of f
+  np   <-  dim(f)[2] # col of f
+  indx <- which(f > 0)
+  ni   <- prod(dim(indx))
+  tf   <- matrix(0, nrep, ni)
+  tc   <- matrix(0, nrep, ns*nt)  
+
+  cc1 <- matrix(0,ns,nt) 
+
+
+  
 # disp(' ')
 # disp('   Time      rmsx      rmsxwt   itr/1000  conv*1e6')
 # tic
@@ -66,29 +86,106 @@ bootnp <- function(s,ssd,f,fsd) {
 #     cc1(ind,:)=cc;
 #     tc(i,:)=cc1(:)';
 # disp([toc,info.rmsx,info.rmsxwt,info.itr/1000,info.conv*1e6])
-# end
-# 
+# end# disp(' ')
+  
+  tictoc::tic.clearlog()
+  start <- tictoc::tic()
+  
+  # references another function
+  # TODO: source(paste0(root,"/scripts/nnmatfactsd.R"))
+  
+  for (i in 1:nrep) {
+    ind <- pracma::ceil(matrix(runif(ns), ns, 1) * ns)
+    temp   <- nnmatfactsd(s[ind,], ssd[ind,], f, fsd, 
+                          info = data.frame(printitr = 1e6))
+    cc     <- temp$cc
+    ff     <- temp$ff
+    info   <- temp$info
+    
+    tf[i,] <- Conj(ff[indx])
+    cc1[,] <- NA
+    cc1[ind,] <- cc
+    tc[i,]=Conj(cc1)
+    
+    end <- (tictoc::toc(quiet = T))$toc - start
+    print(c(Time   = end,
+            rmsx   = info$rmsx,
+            rmsxwt = info$rmsxwt,
+            itr    = info$itr/1000,
+            conv   = info$conv*1e6))
+  }
+
+ 
+ 
+ 
+   
 # disp(' ')
 # disp('Range of variation in tf and tc')
 # tf1=tf-repmat(mean(tf),nrep,1);
 # disp([min(min(tf1)),max(max(tf1))])
 # tc1=tc-repmat(mean(tc),nrep,1);
 # disp([min(min(tc1(~isnan(tc1)))),max(max(tc1(~isnan(tc1))))])
-# 
+  
+  tf1 <- tf - pracma::repmat(mean(tf), nrep, 1)
+  tc1 <- tc - pracma::repmat(mean(tc), nrep, 1)
+  print('Range of variation in tf and tc')
+  print(c(min(tf1),max(tf1)))
+  print(c(min(tc1, na.rm = T),max(tc1, na.rm = T)))
+  
 # figure
 # %loglog(mean(tf),std(tf),'o')
 # loglog(mean(tf),std(tf)./mean(tf),'o')
 # title('Non parametric bootstrap: Variation in f coefficients')
 # xlabel('Mean coefficient value')
 # ylabel('standard deviation/mean')
-# 
+  
+  # allow loglog style plotting from Matlab
+  fancy_scientific <- function(l) {
+    # turn in to character string in scientific notation
+    l <- format(l, scientific = TRUE)
+    # quote the part before the exponent to keep all the digits
+    l <- gsub("^(.*)e", "'\\1'e", l)
+    # turn the 'e+' into plotmath format
+    l <- gsub("e", "%*%10^", l)
+    # return this as an expression
+    parse(text=l)
+  }
+  
+  yticks = outer((1:10),(10^(-5:-1)))
+  xticks = outer((1:10),(10^(0:1)))
+
+  # tf
+  df <- data.frame(x = apply(tf, 2, mean),
+                   y = apply(tf, 2, function(x) sd(x)/mean(x)))
+  
+  ggplot() +
+    # original had bubble dots - shape?
+    geom_point(data = df,
+               aes(x = x,
+                   y = y),
+               colour = "red") +
+    scale_x_log10(limits = c(1, 100),
+                  labels = fancy_scientific,
+                  minor_breaks = xticks) +
+    scale_y_log10(limits = c(1e-5, 1),
+                  labels = fancy_scientific,
+                  minor_breaks = yticks) +
+    labs(
+      title = "Parametric Bootstrap, log normal:  Variation in f coefficients",
+      x     = "Mean coefficient value",
+      y     = "standard deviation/mean"
+    ) +
+    theme_bw()
+  
 # mtc=zeros(ns*nt,1);
 # stc=zeros(ns*nt,1);
 # for i=1:ns*nt
-# t=tc(~isnan(tc(:,i)),i);
-# mtc(i)=mean(t);
-# stc(i)=std(t);
+#   t=tc(~isnan(tc(:,i)),i);
+#   mtc(i)=mean(t);
+#   stc(i)=std(t);
 # end
+
+
 # figure
 # %loglog(mtc',stc,'.')
 # loglog(mtc,stc./mtc,'.')
@@ -99,3 +196,5 @@ bootnp <- function(s,ssd,f,fsd) {
 # return
 # end
 }
+
+
