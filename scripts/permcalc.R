9c5f4d504bbc9434cc1844fd59e3f5be8d660e38
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2022-08-25 15:30:58 -0400

    edit naming

--- scripts/permcalc.R
+++ scripts/permcalc.R
@@ -1,32 +1,35 @@
-permcalc <- function(a,b) {
+permcalc <- function(ratio_pig_filt, df_pig) {
 ################################################################################
 #                                                                              # 
-#               Calculate permutation to term a into b                         #
+#               Calculate permutation to turn a into b                         #
 #                                                                              #    
 ################################################################################
 # DESCRIPTION:
 # --------
-# This uses a vector of pigment names, `a`, that has been pre-filtered using an 
-# index of 0/1 to select only the pigments wanted and uses these names to  
-# identify the pigment names from the data set, `b`, that match. 
+# This uses a vector of pigment names, `ratio_pig_filt`, that has been 
+# pre-filtered using an index of 0/1 to select only the pigments wanted and uses 
+# these names to  identify the pigment names from the data set, `df_pig`, that 
+# match.
 #
-# If a pigment in `a` does not have a match in `b`, then a print message of  
-# "{x name} is not found" will be displayed.
+# If a pigment in `ratio_pig_filt` does not have a match in `df_pig`, then a 
+# print message of "{x name} is not found" will be displayed. Also, a `stop` is
+# initiated with a matrix of the selected pigments and which sample pigments 
+# were not found.
 #
 # Once the search is completed, the matching names are put into an output vector 
-# `p` to be used on the data set for indexing.
+# `new_idx` to be used on the data set for indexing.
 #
 # --------
 # INPUTS:
 # --------
-# a       = vector of pigment names used that are filtered by an index variable 
-#           of 0/1 (0 = not used, 1 = used) in the analysis
-# b       = vector of column names from the data set used
+# ratio_pig_filt  = vector of pigment names used that are filtered by an index  
+#                   variable of 0/1 (0 = not used, 1 = used) in the analysis
+# df_pig          = vector of column names from the data set used
 # 
 # --------
 # OUTPUTS:
 # --------
-# p       = the selected names in `b` that matches `a`
+# new_idx         = the selected names in `df_pig` that matches `ratio_pig_filt`
 # 
 # --------
 # NOTES:
@@ -40,47 +43,35 @@ permcalc <- function(a,b) {
 # Author:
 # --------
 # Sebastian Di Geronimo (Fri Jun 17 15:10:26 2022)
-    
-  
-  # function p=permcalc(a,b)
-  # % permcalc Calculate permutation to term a into b
-  # %  2010-02-09  Matlab7  W.Whiten
-  # 
-  # n=length(a);
-  # p=zeros(1,n);
-  
-  n <- length(a)
-  p <- matrix(0, 1, n)
 
-  # for i=1:length(a)
-  # t=strmatch(a{i},b,'exact');
-  # if(isempty(t))
-  #   disp([a{i} '  not found'])
-  # else
-  #   p(i)=t;
-  # end
-  # end
-  # 
-  # return
-  # end
+  n       <- length(ratio_pig_filt) # number of pigments used in analysis
+  new_idx <- matrix(0, 1, n) 
   
-  for (i in 1:n) {
+  for (i in seq(n)) {
     
     # t is the index in b that matches a
-    t <- which(b == a[i])
+    match_idx    <- which(df_pig == ratio_pig_filt[i])
     
     # if does not match, will message "is not found"
-    if (identical(t,integer(0))) {
-      
-      message(paste(a[i], "is not found"))
+    if (identical(match_idx,integer(0))) {
+
+      message(paste(ratio_pig_filt[i], "is not found"))
+      new_idx[i] <- NA
       
     } else {
       
-      # if matches will add index in b to p
-      p[i] <- t
+      # if matches adds index in df_pig to new_idx
+      new_idx[i] <- match_idx
       
     }
   }
+
+  if (!identical(ratio_pig_filt, df_pig[new_idx])) {
+    message('Names do not match')
+    print(cbind("Selected Pigments"= ratio_pig_filt,
+                "Sample Pigments"  = df_pig[new_idx]))
+    stop("Look at list of non-matching names!")
+  }
   
-  return(p)
+  return(new_idx)
 }
