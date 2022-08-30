6d641cc449e9a0cbc21e0e48646a9e962df6970f
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2022-08-30 20:29:39 +0000

    edit header

--- scripts/normprod.R
+++ scripts/normprod.R
@@ -1,46 +1,32 @@
-normprod <- function(s,c,f) {
+normprod <- function(.df,c,f) {
 ################################################################################
 #                                                                              # 
-#               Normalise matrix product on last row of s & f                  #
+#               Normalize matrix product on last row of s & f                  #
 #                                                                              #    
 ################################################################################
-# DESCRIPTION:
-# --------
+# ---- DESCRIPTION: -------
 #
-# --------
-# INPUTS:
-# --------
-# s       = Original product matrix
-# c       = Left factor of s
-# f       = Right factor of s
+# ---- INPUTS: -----------
+# df = Original product matrix
+# c = Left factor of df
+# f = Right factor of df
 #
-# --------
-# OUTPUTS:
-# --------
-# ss      = Matrix s rescaled for one in last column
-# cc      = Left factor after scaling
-# ff      = Right factor after scaling
-# rms     = Root mean square of ss-cc*ff
+# ---- OUTPUTS: ----------
+# ss = Matrix .df rescaled for one in last column
+# cc = Left factor after scaling
+# ff = Right factor after scaling
+# rms = Root mean square of ss-cc*ff
 #
-# --------
-# NOTES:
-# --------
+# ---- NOTES: ------------
 # Original: 2010-03-28  Matlab7  W.Whiten
 #
-# --------
-# References:
-# --------
+# ---- REFERENCES(s): ----
 #
-# --------
-# Author:
-# --------
+# ---- AUTHOR(s): --------
 # Sebastian Di Geronimo (Sun Jun 19 14:53:10 2022)
  
-  # function [ss,cc,ff,rms]=normprod(s,c,f)
-  # % normprod  Normalise matrix product on last row of s & f
   # %  2010-03-28  Matlab7  W.Whiten
-  # %
-  # % [ss,cc,ff,rms]=normprod(s,c,f)
+  
   # %  s   Original product matrix
   # %  c   Left factor of s
   # %  f   Right factor of s
@@ -55,15 +41,15 @@ normprod <- function(s,c,f) {
   # [ns,np]=size(s);
   # nt=size(f,1);
   
-  ns <- dim(s)[1]
-  np <- dim(s)[2]
+  ns <- dim(.df)[1]
+  np <- dim(.df)[2]
   nt <- dim(f)[1]
   
   # s1=1./(s(:,end)+1e-100);
   # ss=s.*repmat(s1,1,np);
   
-  s1 <- as.matrix(1 / (s[,ncol(s)] + 1e-100))
-  ss <- s * pracma::repmat(s1, 1, np)
+  s1 <- as.matrix(1 / (.df[,ncol(.df)] + 1e-100))
+  ss <- .df * pracma::repmat(s1, 1, np)
   
   # s2=f(:,end);
   # ff=f.*repmat(1./(s2+1e-100),1,np);
