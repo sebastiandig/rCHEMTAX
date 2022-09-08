b34edd5a46f7f5ca5f544ea3f494e4591f41ec00
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2022-09-08 19:37:07 +0000

    moved bottom end to another script, similar to brokewest.R

--- scripts/CHEMTAXBROKEWestb.R
+++ scripts/CHEMTAXBROKEWestb.R
@@ -1,118 +1,126 @@
-library("tictoc")
-library("ggplot2")
-library("pracma")
-
-root <- rprojroot::find_rstudio_root_file()
-source(paste0(root,"/scripts/permcalc.R"))
-# TODO: matfactuvw
-source(paste0(root,"/scripts/matfactuvw.R"))
-
-# bs=[1	0	0	1	1	0	1	1	1	1	1	0	1	1	0	0	1	0	1];
-# bb={'chlc3'	'MgDVP'	'chlc2'	'chlc1'	'per'	'but'	'fuc'	'neox'	'prx'	'violax'	'hex'	'Mmal'	'alx'	'lut'	'dhlut'	'GyroxTotal'	'chl_b'	'np_chl_c2'	'chl_a'};
-# b=[0	0.082815484	0	0	0	0	0	0.076017708	0.093775606	0.048498574	0	0.034069268	0	0.006383245	0.023810763	0	0.663053868	0	1
-#    0	0.001674811	0	0	0	0	0	0.074384251	0	0.036350279	0	0	0	0.220905086	0	0	0.167089565	0	1
-#    0	0.001280276	0.14910557	0	0	0	0	0	0	0	0	0	0.224584718	0	0	0	0	0	1
-#    0	0.001108976	0.076997483	0.15	0	0	0.8	0	0	0	0	0	0	0	0	0	0	0	1
-#    0.033	0	0.131	0	0	0	0.61	0	0	0	0	0	0	0	0	0	0	0	1
-#    0	0.000974879	0.367132108	0	0.876791629	0	0	0	0	0	0	0	0	0	0	0	0	0	1
-#    0.13	0.001284182	0.023	0	0	0.01	0.08	0	0	0	0.4	0	0	0	0	0	0	0.03	1
-#    0.27	0.001120303	0.16	0	0	0.12	0.01	0	0	0	1.1	0	0	0	0	0	0	0.06	1];
-
-# turns columns on/off (1/0) to be used in analysis
-bs <- c(1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1)
-# bb <- c("chlc3", "MgDVP", "chlc2", "chlc1", "per", "but", "fuc", "neox", "prx", 
-        # "violax", "hex", "Mmal", "alx", "lut", "dhlut", "GyroxTotal", "chl_b",
-        # "np_chl_c2", "chl_a")
-
-# reads pigment ratio start file and extracts col names
-b <- as.matrix(read.csv(paste0(root,"/scripts/brokewest_b_pigment_ratios.csv")))
-bb <- colnames(b)
-colnames(b) <- NULL
-
-# CHEMTAXBROKEWestx
-# should just be a file to load
-# i.e 
-x <- as.matrix(read.csv(paste0(root,"/data/raw/CHEMTAXBROKEWestx.csv")))
-xx <- colnames(x)
-colnames(x) <- NULL
-
-# xi=permcalc(bb(bs==1),xx);
-# bi=find(bs);
-# bb(bi)
-# xx(xi)
-
-# x1=x(:,xi);
-# b1=b(:,bi);
-# x2=x1./repmat(sum(x1,2),1,size(x1,2));
-# b2=b1./repmat(sum(b1,2),1,size(b1,2));
-
-xi <- permcalc(bb[bs == 1], xx)
-bi <- which(bs == 1)
-bb[bi]
-xx[xi]
-
-x1 <- x[,xi]
-b1 <- b[,bi]
-x2 <- x1/pracma::repmat(as.matrix(apply(x1, 1, sum, na.rm = TRUE)),1,ncol(x1))
-b2 <- b1/pracma::repmat(as.matrix(apply(b1, 1, sum, na.rm = TRUE)),1,ncol(b1))
-
-
-# ---- Everything up to here works as expected ----
-
-
-
-# tic;[aaa,bbb,info]=matfactuvw(x2,b2,1,struct('maxitr',2000));toc
-# %[aaa,bbb,info]=matfact5(x2,b2,struct('maxitr',200));
-
-
-tictoc::tic()
-temp <- matfactuvw(x = x2, b = b2, 1, info =list(maxitr = 2000))
-aaa  <- temp$a
-bbb  <- temp$b
-info <- temp$info
-tictoc::toc()
-
-
-# t=x1(:,end)<0.3;
-# x3=x2(t,:);
-
-t  <- x1[,ncol(x1)] < 0.3
-x3 <- x2[t,]
-
-# semilogy(x1(:,end))
-# title('CHEMTAXBROKEWest  Ch_a')
-# xlabel('Sample number')
-# ylabel('Chl-a')
-
-x1.name <- colnames(x1)[ncol(x1)]
-
-# allow y log style plotting from Matlab in semilogy
-fancy_scientific <- function(l) {
-  # turn in to character string in scientific notation
-  l <- format(l, scientific = TRUE)
-  # quote the part before the exponent to keep all the digits
-  l <- gsub("^(.*)e", "'\\1'e", l)
-  # turn the 'e+' into plotmath format
-  l <- gsub("e", "%*%10^", l)
-  # return this as an expression
-  parse(text=l)
+chemtaxbrokewest_b <- function(idx = c(1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1)) {
+################################################################################
+#                                                                              # 
+#           ---- Read phytoplankton data for chemtaxbrokewestb ----            #
+#                                                                              #    
+################################################################################
+# ---- DESCRIPTION: ------
+# The CHEMTAXBROKEWestx.csv is read that contains the pigment data for all
+# measured pigments (rows = sample, col = pigment). The
+# brokewest_b_pigment_ratios.csv is read that contains the initial pigments
+# ratio matrix (row = taxa ratio, col 1 = species name, others = pigment ratio).
+# The pigments are selected by an index of 1/0 representing the cols in the
+# pigment ratio matrix that is used. This is pre-selected for testing. A
+# comparison between pigments names from pigment ratio matrix and sample matrix
+# is done to select the same pigments (spelling counts) and check that none is
+# missing from the sample matrix. Two matrices are created for initial standard
+# deviation using the formula(s):
+# 
+# df_norm    = df / row sum df
+# pig_r_nrom = pig_r / row sum pig_r
+# 
+# ---- INPUTS: -----------
+# idx        = index of selected and deselected pigment from pigment ratio matrix
+#
+# ---- OUTPUTS: ----------
+# df         = Matrix of samples by pigment readings
+# df_norm    = Normalized df to row sums
+# pig_r_init = Initial pigment ratio matrix by taxa
+# pig_r_norm = Normalized pigment ratio matrix to row sums
+# taxa       = Cell array of taxa names
+# pigm_sel   = Cell array of selected pigment names 
+#
+# ---- NOTES: ------------
+#
+# ---- REFERENCES(s): ----
+#
+# ---- AUTHOR(s): --------
+# Sebastian Di Geronimo (2022-09-08 18:59:47)  
+  # ---- load library ----
+  library("pracma")
+  
+  # ---- set directory ----
+  root <- rprojroot::find_rstudio_root_file()
+  raw  <- "/data/raw/"
+  source(paste0(root, "/scripts/permcalc.R"))
+  # TODO: matfactuvw
+  source(paste0(root, "/scripts/matfactuvw.R"))
+  
+  # ---- read data ----
+  # CHEMTAXBROKEWestx pigment concentration
+  # TODO: should make this a function?
+  sample_filepath <- paste0(root, raw, "CHEMTAXBROKEWestx.csv")
+  df              <- as.matrix(read.csv(sample_filepath))
+  
+  # pigment ratio matrix
+  pig_ratio_filepath   <-
+    paste0(root, "/scripts/brokewest_b_pigment_ratios.csv")
+  temp_ratio           <- read.csv(pig_ratio_filepath)
+  
+  # extract pigment names
+  df_pig               <- colnames(df)
+  pigm_temp <- colnames(temp_ratio)[-1]
+  
+  colnames(df)         <- NULL # TODO: decide if it matters or not
+  colnames(temp_ratio) <- NULL # TODO: decide if it matters or not
+  
+  # extract taxa names
+  taxa                 <- temp_ratio[, 1]
+  
+  # ---- index for selected pigments of pigment ratios matrix ----
+  if (is.null(idx) | length(idx) != length(pigm_temp)) {
+    message(
+      paste0(
+        "Either an index was not supplied, was 0, or exceeded the number of pigments!\n",
+        "\nHere are a list of pigment names:\n"
+      )
+    )
+    message(paste(pigm_temp, "\n"))
+    message(paste0("Give a 1 or a 0 for each name\n",
+                   "(1 = selected; 0 = not selected)\n"))
+    idx <- readline(prompt = paste0("Should be a length of ",
+                                    length(pigm_temp),
+                                    " (ex 1 0 1 1 0):\n "))
+    idx <- as.integer(strsplit(idx, " ")[[1]])
+    if (length(idx) != length(pigm_temp)) {
+      idx <-  NULL
+      stop("Input does not match length of pigments")
+    }
+    message(paste0("\nVerify selected pigments are correct:\n"))
+    cat(pigm_temp[which(idx == 1)])
+  }
+  
+  # ---- read pigment ratios ----
+  # initialize ratio matrix by removing taxa and pigment names
+  pig_r_init           <-
+    as.matrix(temp_ratio[, -1])[, which(idx == 1)]
+  
+  # extract selected pigment names
+  pigm_sel             <- pigm_temp[which(idx == 1)]
+  
+  # ---- find columns that match and rearrange ----
+  df_pig_idx <- permcalc(pigm_sel, df_pig)
+  
+  # filter columns for pigment that match selected pigments
+  df  <- df[, df_pig_idx]
+  
+  # ---- normalize df and pigment ratio to row sums ----
+  df_row_sum    <- as.matrix(apply(df, 1, sum, na.rm = TRUE))
+  pig_r_row_sum <-
+    as.matrix(apply(pig_r_init, 1, sum, na.rm = TRUE))
+  
+  df_norm       <- df / pracma::repmat(df_row_sum, 1, ncol(df))
+  pig_r_norm    <- pig_r_init / pracma::repmat(pig_r_row_sum, 1, ncol(b1))
+  
+  # ---- return list of variables created ----
+  result <-
+    list(
+      df         = df,
+      df_norm = df_norm,
+      pig_r_init = pig_r_init,
+      pig_r_norm = pig_r_norm ,
+      taxa       = taxa,
+      pigm_sel   = pigm_sel
+    )
+  
 }
-
-  yticks = outer((1:10),(10^(-5:-1)))
-  xticks = outer((1:10),(10^(0:1)))
-  
-  
-ggplot(x1, aes(y = !!sym(x1.name), x = seq(1, nrow(x1)))) +
-  geom_point() +
-  labs(title = 'CHEMTAXBROKEWest  Ch_a',
-       x = 'Sample number',
-       y ='Chl-a') +
-  scale_y_log10(limits = c(1e-5, 1),
-                labels = fancy_scientific,
-                minor_breaks = yticks) +
-  theme_bw()
-
-
-
-
-
